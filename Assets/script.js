68a5f20f29e08186f34faa19f1729423937282f6
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-01 14:48:45 -0600

    added variables for questions and time

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,26 @@
 var startEl = document.querySelector("#startButton");
 var headerEl = document.querySelector("#Heading");
+var Questions = [
+  {
+    title: "First Questions title",
+    choices: ["Choice A", "Choice B", "Choices C"],
+    answer: "Choice B",
+  },
+  {
+    title: "First Questions title",
+    choices: ["Choice A", "Choice B", "Choices C"],
+    answer: "Choice B",
+  },
+];
+var timer = document.getElementById("timer");
+var time = 0;
 
 function startQuiz() {
+  timer.textContent = 70;
   headerEl.textContent = "Go!!!";
-  console.log
+  console.log;
 }
 
 startEl.addEventListener("click", startQuiz);
+
+//  
