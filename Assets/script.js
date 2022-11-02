3604b26bca7a3ddc9c10fcf665a73b18c9deb4c9
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-01 23:20:12 -0600

    added another question

--- Assets/script.js
+++ Assets/script.js
@@ -14,17 +14,33 @@ var Questions = [
     choices: ["no", "no", "no", "LETS PLAY"],
     answer: "LETS PLAY",
   },
+  {
+    title: "Do you like coding?",
+    choices: ["YES", "no", "...", "maybe"],
+    answer: "YES",
+  },
 ];
 
 var timer = document.getElementById("timer");
 var time = 0;
 var answerArea = document.getElementById("placeButtons");
 var makeBtn = document.body.querySelector("#placeButtons");
+var questionAmount = Questions.length + 1;
+var currentQuestion;
 
 function startQuiz() {
+  //need to find out if a questions has been asked, remove that question from the array of questsions that can still be asked
+  console.log("questoin amout is starting at " + questionAmount);
+  questionAmount = questionAmount - 1;
+  console.log("question ammount is now" + questionAmount);
+  if (questionAmount <= 0) {
+    alert("ran out");
+  }
   timer.textContent = 70;
-  var randomChoice = Math.floor(Math.random() * Questions.length);
-  var currentQuestion = Questions[randomChoice];
+  const randomChoice = Math.floor(Math.random() * Questions.length);
+  currentQuestion = Questions[randomChoice];
+  console.log(Questions);
+
   answerArea.textContent = "";
 
   headerEl.textContent = currentQuestion.title;
@@ -66,40 +82,56 @@ function startQuiz() {
   answerArea.children[0].addEventListener("click", function () {
     userChoice = answerArea.children[0].getAttribute("status");
     if (userChoice === "Correct") {
-      console.log("ya thats right !");
+      updateQuestions();
+
       startQuiz();
+      console.log("ya thats right !");
     } else {
       console.log("did not work");
     }
     console.log(userChoice);
   });
+
   answerArea.children[1].addEventListener("click", function () {
     userChoice = answerArea.children[1].getAttribute("status");
     if (userChoice === "Correct") {
-      console.log("ya thats right !");
+      updateQuestions();
+
       startQuiz();
+      console.log("ya thats right !");
     } else {
       console.log("did not work");
     }
   });
+
   answerArea.children[2].addEventListener("click", function () {
     userChoice = answerArea.children[2].getAttribute("status");
     if (userChoice === "Correct") {
-      console.log("ya thats right !");
+      updateQuestions();
+
       startQuiz();
+      console.log("ya thats right !");
     } else {
       console.log("did not work");
     }
   });
+
   answerArea.children[3].addEventListener("click", function () {
     userChoice = answerArea.children[3].getAttribute("status");
     if (userChoice === "Correct") {
-      console.log("ya thats right !");
+      updateQuestions();
+
       startQuiz();
+      console.log("ya thats right !");
     } else {
       console.log("did not work");
     }
   });
+  function updateQuestions() {
+    console.log(currentQuestion);
+    Questions.slice(currentQuestion, "1");
+  }
 }
+function quizAgain() {}
 
 startEl.addEventListener("click", startQuiz);
